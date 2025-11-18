307b2439b01b77f6fa1470e94c9bb60b45b1c828
Altesse Royal <altessemwamboko@gmail.com>
2025-11-18 14:31:45 +0100

    Update handler.js
    Signed-off-by: Altesse Royal <altessemwamboko@gmail.com>

--- handler.js
+++ handler.js
@@ -1,3 +1,5 @@
+// handler.js
+
 /**
  * Gère tous les messages entrants.
  * @param {Object} client L'objet client Baileys
@@ -19,6 +21,8 @@ export default async function handler(client, m) {
     // Exemple de commande simple
     if (command === 'ping') {
         const uptime = process.uptime();
+        // client.sendText est une fonction que nous avons restaurée dans index.js
         await client.sendText(m.chat, `Pong! 🚀\nUptime: ${uptime.toFixed(2)} secondes`, m);
     }
+    // Ajoutez ici toutes vos autres commandes
 }
